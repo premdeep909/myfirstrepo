9aa560f3dc01e0713ef237dac16123db5cc41cf0
premdeep909 <premdeep909@gmail.com>
2023-12-05 11:34:35 +0530

    Third Commit

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const heading = document.querySelector("h1");
-heading.style.backgroundColor = "red";
+heading.style.backgroundColor = "blue";
+const heading2 = document.querySelector("h2");
+heading2.style.backgroundColor = "green";
